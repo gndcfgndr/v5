b034d25edd162b8e20db45b9448ee94d608f3d1d
myvpn1 <137178860+myvpn1@users.noreply.github.com>
2023-12-17 23:32:07 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -229,11 +229,11 @@ mkdir -p /usr/local/etc/xray
 
 # // String / Request Data
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-DOMAIN=storeid.cloud
-SUB_DOMAIN=${sub}.storeid.cloud
-NS_DOMAIN=ns.${sub}.storeid.cloud
-CF_ID=ilhamstoress726@gmail.com
-CF_KEY=6159615642:AAEoAjxKYMwN81Pcgas7b-F2GFYRB0kQomU
+DOMAIN=klmpk.me
+SUB_DOMAIN=${sub}.klmpk.me
+NS_DOMAIN=ns.${sub}.klmpk.me
+CF_ID=andyyuda41@gmail.com
+CF_KEY=0d626234700bad388d6d07b49c42901445d1c
 set -euo pipefail
 IP=$(curl -sS ifconfig.me);
 echo "Updating DNS for ${SUB_DOMAIN}..."
