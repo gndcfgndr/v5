46067ef2f2b79b2a7c09bee260bbbcd293962d23
gndcfgndr <david12.34343243@gmail.com>
2025-02-21 18:22:51 +0200

    Replace old URL with new URL

--- usernew.sh
+++ usernew.sh
@@ -41,7 +41,7 @@ CITY=$(cat /etc/xray/city)
 domain=$(cat /etc/xray/domain)
 
 BURIQ () {
-    curl -sS https://raw.githubusercontent.com/myvpn1/izin/main/ip > /root/tmp
+    curl -sS https://raw.githubusercontent.com/gndcfgndr/izin/main/ip > /root/tmp
     data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
     for user in "${data[@]}"
     do
@@ -59,7 +59,7 @@ BURIQ () {
 }
 
 MYIP=$(curl -sS ipv4.icanhazip.com)
-Name=$(curl -sS https://raw.githubusercontent.com/myvpn1/izin/main/ip | grep $MYIP | awk '{print $2}')
+Name=$(curl -sS https://raw.githubusercontent.com/gndcfgndr/izin/main/ip | grep $MYIP | awk '{print $2}')
 echo $Name > /usr/local/etc/.$Name.ini
 CekOne=$(cat /usr/local/etc/.$Name.ini)
 
@@ -76,7 +76,7 @@ fi
 
 PERMISSION () {
     MYIP=$(curl -sS ipv4.icanhazip.com)
-    IZIN=$(curl -sS https://raw.githubusercontent.com/myvpn1/izin/main/ip | awk '{print $4}' | grep $MYIP)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/gndcfgndr/izin/main/ip | awk '{print $4}' | grep $MYIP)
     if [ "$MYIP" = "$IZIN" ]; then
     Bloman
     else
